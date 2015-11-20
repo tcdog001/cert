9948d07bf08cf93dadd88393aa85280c48d3d179
ljf10000 <l_j_f@163.com>
2015-11-20 14:45:58 -0500

    .

--- lss/cert.sh
+++ lss/cert.sh
@@ -8,19 +8,34 @@ prepare() {
 	echo 01 > demoCA/serial
 }
 
-openssl genrsa -des3 -out server.key 1024
-openssl rsa -in server.key -out server.key
-openssl req -new -key server.key -out server.csr -config openssl.cnf
+private() {
+	local name="$1"
 
-openssl genrsa -des3 -out client.key 1024
-openssl rsa -in client.key -out client.key
-openssl req -new -key client.key -out client.csr -config openssl.cnf
+	openssl genrsa -des3 -out ${name}.key 1024
+	openssl rsa -in ${name}.key -out ${name}.key
+	openssl req -new -key ${name}.key -out ${name}.csr -config openssl.cnf
+	cat ${name}.key | base64 > ${name}.key.base64
+}
 
-openssl req -new -x509 -keyout ca.key -out ca.crt -config openssl.cnf
-openssl rsa -in ca.key -out ca.key
+cert() {
+	local name="$1"
 
-prepare
-openssl ca -in server.csr -out server.crt -cert ca.crt -keyfile ca.key -config openssl.cnf
-prepare
-openssl ca -in client.csr -out client.crt -cert ca.crt -keyfile ca.key -config openssl.cnf
+	prepare
+	openssl ca -in ${name}.csr -out ${name}.crt -cert ca.crt -keyfile ca.key -config openssl.cnf
+	cat ${name}.crt | base64 > ${name}.crt.base64
+}
 
+main() {
+	private server
+	private client
+
+	openssl req -new -x509 -keyout ca.key -out ca.crt -config openssl.cnf
+	openssl rsa -in ca.key -out ca.key
+	cat ca.key | base64 > ca.key.base64
+	cat ca.crt | base64 > ca.crt.base64
+
+	cert server
+	cert client
+}
+
+main "$@"
